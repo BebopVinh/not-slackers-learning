9bb1606d328b9ccb022209d25930504c413db001
bebopvinh <vinhdo07@gmail.com>
2020-04-02 02:43:19 -0700

    remove boilerplate and customize for cohort

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,30 +1,33 @@
 import React from 'react'
 import './card.css'
 
-const Card = ({
-    title = "Mount Raineer",
-    elevation = 4000,
-    src = "https://tailwindcss.com/img/card-top.jpg"
-}) => (
+const Card = ({ props }) => {
+  const { days, description, imgUrl, lead, tags, time, title } = props
+  // debugger
+  return (
     <div className="paper">
-        <div className="w-full h-64 bg-cover" style={{
-            backgroundImage: `url(${src})`
-        }} />
-        <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">{title}</div>
-            <p className="text-grey-darker text-base">
-                {elevation}
-            </p>
-        </div>
-        <div className="px-6 py-4">
-            <span
-                className="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#photography</span>
-            <span
-                className="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#travel</span>
-            <span
-                className="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#mountains</span>
-        </div>
+      {/* <div
+        className="w-full h-64 bg-cover"
+        style={{
+          backgroundImage: `url(${imgUrl})`,
+        }}
+      /> */}
+      <img className="w-full min-h-1/4" src={imgUrl} alt="header image" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{title}</div>
+        <p className="text-grey-darker text-base mb-4">{lead}</p>
+        <p class="text-gray-700 text-base">{description}</p>
+      </div>
+
+      <div className="px-6 py-4">
+        {tags.map(tag => (
+          <span className="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">
+            {tag}
+          </span>
+        ))}
+      </div>
     </div>
-)
+  )
+}
 
 export default Card
