525c9f70ef8af48ec5d21bf9da9177528240c947
bebopvinh <vinhdo07@gmail.com>
2020-04-02 02:59:59 -0700

    change site's title and browser tab icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby and GraphCMS',
+    title: '!Slackers Learning Together!',
   },
-  pathPrefix: "/gatsby-graphcms-tailwindcss-example",
+  pathPrefix: '/gatsby-graphcms-tailwindcss-example',
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-postcss',
@@ -12,18 +12,19 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/not-slackers-logo-tab.png', // This path is relative to the root of the site.
       },
     },
     {
-      resolve: "gatsby-source-graphql",
+      resolve: 'gatsby-source-graphql',
       options: {
         // This type will contain remote schema Query type
-        typeName: "GCMS",
+        typeName: 'GCMS',
         // This is field under which it's accessible
-        fieldName: "gcms",
+        fieldName: 'gcms',
         // Url to query from
-        url: "https://api-euwest.graphcms.com/v1/cjm7tab4c04ro019omujh708u/master",
+        url:
+          'https://api-euwest.graphcms.com/v1/cjm7tab4c04ro019omujh708u/master',
       },
     },
     'gatsby-plugin-offline',
