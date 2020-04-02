2b8c68d3d9a512a2fb097c61fa9ca2494da2e18e
bebopvinh <vinhdo07@gmail.com>
2020-04-02 02:45:50 -0700

    remove boiler plate and customize for cohort

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,15 @@
-
 import React from 'react'
 import Layout from '../components/layout'
 import { StaticQuery, graphql } from 'gatsby'
 import Card from '../components/Card'
 
+import { eventsArray } from '../data/events'
+
 const IndexPage = () => (
   <Layout>
-  <StaticQuery
-    query={graphql`
-      query {
-        gcms {
-          mountains {
-            title
-              elevation
-              image {
-                url
-              }
-          }
-        }
-      }
-    `}
-    render={data => {
-      if (!data.gcms) {
-        return <p>Loading…</p>
-      }
-      return data.gcms.mountains.map(d =>
-        <Card
-          title={d.title}
-          elevation={d.elevation}
-          src={d.image ? d.image.url : undefined}
-        />
-        )
-    }}
-    />
+    {eventsArray.map(day => {
+      return <Card props={day} />
+    })}
   </Layout>
 )
 
